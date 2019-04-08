c15ca66f42a873c08e75b5f084eecf53c5375bd1
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-08 23:50:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   def match(array_words)
     array= []
     array_words.each do|word|
-      if select{@word.split("").sort==word.split("").sort}
+      if @word.split("").sort==word.split("").sort
       #binding.pry
         return [word]
       end
