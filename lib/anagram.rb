03dcb4d448f104d3488498440f20b446b2088627
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-08 23:49:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,8 @@ class Anagram
   def match(array_words)
     array= []
     array_words.each do|word|
-      if select@word.split("").sort==word.split("").sort
-      binding.pry
+      if select{@word.split("").sort==word.split("").sort}
+      #binding.pry
         return [word]
       end
     end
