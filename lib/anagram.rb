173ecd84eea013059c96103e84043b9c6e64f175
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-09 00:14:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   
   def match(array_words)
    
-    array_words.select{@word.split("").sort==word.split("").sort}
+  array_words.select{@word.split("").sort==word.split("").sort}
      # binding.pry
       
   end
